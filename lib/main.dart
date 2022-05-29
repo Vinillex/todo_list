5057965183910f806e2283852693df624b171edb
Vinillex <shettigarvinit23299@gmail.com>
2022-05-29 16:09:39 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'dart:core';
+
+import 'package:intl/intl.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:untitled/screens/notes_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const String title = 'Notes SQLite';
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: title,
+    themeMode: ThemeMode.dark,
+    theme: ThemeData(
+      primaryColor: Colors.black,
+      scaffoldBackgroundColor: Colors.blueGrey.shade900,
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+    ),
+    home: NotesPage(),
+  );
+}
